5ec14de377dac77add957f24675987ea0258bb81
Gianluca Fava <101229939+Gianluca1796@users.noreply.github.com>
2023-08-15 21:06:05 -0300

    clase1

---
+++ js/clase1.js
@@ -0,0 +1,60 @@
+/// *** VARIABLES *** //
+//let vairble -> Declaración
+// let variable = valor --> Asignación del valora de la variable
+
+
+// LET
+// let nombre = "Gianluca"
+// let bool = true;
+// let edad = 27
+// let apellido; 
+// console.log(apellido)
+// apellido = "Fava"
+// console.log(apellido)
+// apellido = 14
+// console.log(apellido)
+
+
+
+//CONST
+// const alumno = "Juan Perez"
+// const curso = "JavaScript"
+// curso = "Desarrollo Web"  --> No se puede reasignar un valor a una constante
+
+
+
+//OPERACIONES BASICAS
+
+// let valorVuelo = parseFloat(prompt("Ingrese el valor del vuelo"))
+// let impuestos = parseFloat(prompt("Ingrese el valor del impuesto"))
+// let suma = valorVuelo + impuestos
+// alert(`El valor de la suma es ${suma}`)
+
+
+//CONCATENACIÓN
+// let nombre = "Ivan"
+// let apellido = "Abruzzese"
+// let nombreCompleto = nombre + " " + apellido 
+// let nombreCompleto = `Hola ${nombre} ${apellido}! Bienvenido a nuestro curso!` --> Template String
+// console.log(nombreCompleto)
+
+
+// let confirmacion = confirm("¿Desea aceptar los términos y condiciones?")
+// console.log(confirmacion)
+
+
+// let precioProducto = parseFloat(prompt("Ingrese el precio de su producto"))
+
+// const IVA = 1.21
+
+// let precioConIva = precioProducto * IVA
+
+// alert(`El precio final de venta de su producto es de ${precioConIva}`)
+
+
+// let nombreDelProducto = prompt("Ingrese el nombre del producto")
+// let cantidadDelProducto = prompt("Ingrese la cantidad del stock producto")
+// let categoriaDelProducto = prompt("Ingrese la categoria del stock producto")
+
+
+// alert("Haz registrado" + " " + cantidadDelProducto + " " + " de" +  nombreDelProducto + " en la categoría" + " " + categoriaDelProducto)
